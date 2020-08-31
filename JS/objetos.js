98187ba39668560be4da46da0fc99384d0a2fedf
franchuf <franchuf@gmail.com>
2020-08-31 03:50:23 -0300

    mas js

---
+++ JS/objetos.js
@@ -0,0 +1,8 @@
+var carro = {
+    marca:"toyota",
+    modelo:"corolla",
+    annio:2020,
+    detalleDelAuto: function(){
+        console.log(`auto ${this.marca}, ${this.modelo}, ${this.annio} `)
+    }
+}
