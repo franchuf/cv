94d056b023f03ab19f6f0ce7596cf9b2394512d3
franchuf <franchuf@gmail.com>
2020-08-31 17:27:52 -0300

    ok

---
+++ JS/funcionCons2.js
@@ -0,0 +1,15 @@
+var discos = [
+    {album: "use your ilusion", costo: 200},
+    {album:"la era de la b", costo: 300},
+    {album:"elotroletravalava", costo: 150},
+];
+
+
+var filtro
+
+
+// function constructora (marca,modelo,anio){
+//     this.marca1 = marca;
+//     this.modelo1 = modelo;
+//     this.anio1 = anio;
+// }
