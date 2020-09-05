c462c21345b6131ee067e53af7dfb49234b9dcba
franchuf <franchuf@gmail.com>
2020-09-05 09:56:30 -0300

    filtros recorrido de matrices

--- JS/funcionCons2.js
+++ JS/funcionCons2.js
@@ -1,11 +1,20 @@
 var discos = [
     {album: "use your ilusion", costo: 200},
     {album:"la era de la b", costo: 300},
-    {album:"elotroletravalava", costo: 150},
+    {album:"elotro0", costo: 223},
+    {album:"elotro1", costo: 333},
+    {album:"elotro2", costo: 150},
+    {album:"elotro3", costo: 233},
+    {album:"elotro4", costo: 120},
+
 ];
 
 
-var filtro
+var filtro = discos.filter(function(articulo){
+    return articulo.costo<=250
+});
+    
+
 
 
 // function constructora (marca,modelo,anio){
