63c5afefe7e912f857564d72186c4aaf9d8c3c03
franchuf <franchuf@gmail.com>
2020-09-10 15:29:16 -0300

    mas platzi

--- JS/2 objetos.js
+++ JS/2 objetos.js
@@ -6,3 +6,21 @@ var carro = {
         console.log(`auto ${this.marca}, ${this.modelo}, ${this.annio} `)
     }
 }
+
+var fran = {
+    nombre: 'francisco',
+    apellido: 'faner',
+    edad: 23,
+}
+
+var nombreYedad = function (persona){
+    //var nombre = persona.nombre; es lo mismoque de ahi abajo
+    var {nombre} = persona
+    var {apellido}=persona
+    var {edad}=persona
+    console.log(`hola me llamo ${nombre} y mi apellido es ${apellido} y tengo ${edad} años`)
+}
+
+function cumpleanios (persona){
+    persona.edad += 1;
+}
--- JS/funciones.js
+++ JS/funciones.js
@@ -1,19 +1,26 @@
-var NombreYEdad = function (n,e){
-    console.log ( ` mi nombre es ${n} y mi edad es ${e}`);
-}
 
 
 
 //lo que se ve aca abajo es que la variable nombre local de la funcion no afecta a la variable global nombre.
-var nombre = "fran"
-var ImprimirNombreEnMayusculas = function (persona){
-    nombreCompleto= persona.nombre.toUpperCase() +' '+ persona.apellido.toUpperCase();
-    console.log(`mi nombre completo es nombreCompleto es ${nombreCompleto}`);
 
-}
+
 
 var Fran = {
     nombre: 'francisco', 
     apellido: 'faner',
     edad: 39,
 }
+
+var NombreYEdad = function (n,e){
+    console.log ( ` mi nombre es ${n} y mi edad es ${e}`);
+}
+
+
+
+var ImprimirNombreEnMayusculas = function ({nombre,apellido}){ //aca con las llaves se interviene adentro del objeto
+    nombreCompleto= nombre.toUpperCase() +' '+ apellido.toUpperCase();
+    console.log(`mi nombre completo es ${nombreCompleto}`);
+
+}
+
+
