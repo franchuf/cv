5338ce693d42758a19fe2e86f147cf0b380a4e7a
franchuf <franchuf@gmail.com>
2020-09-14 17:56:42 -0300

    siii

---
+++ JS/18. Switch.js
@@ -0,0 +1,13 @@
+var signo = prompt('pone tu signo por favor');
+
+switch (signo) {
+    case 'aries':
+        'aries blabjakkjlsdjkñlafsdjkflañsdjkl';
+        break;
+    case 'virgo':
+        'viergo asdljñlkfasjdklfñadsklfajksl';
+        break
+    default:
+        console.log(`${signo} no existe`);
+
+}
