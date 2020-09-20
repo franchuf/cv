46423c05b41f7cd484df1ea3ae21aa3b547cacd8
franchuf <franchuf@gmail.com>
2020-09-20 09:57:03 -0300

    elimina js

--- JS/1.2While.js
+++
@@ -1,8 +0,0 @@
-var estudiantes = ["maria","juan","pedro","marcos"];
-function saludo(estudiante){
-    console.log(`hola, ${estudiante}`);
-}
-while( estudiantes.length>0 ){
-    var estudiante=estudiantes.shift(); //estudiante.shift es una funcion que extrae el primer elemento del array y lo retorna. Este elemento modifica la longitud del array
-    saludo(estudiante);
-}
